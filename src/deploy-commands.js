1a3aa0ddecf5aa45f53360a3b5c7278f4242a7e9
AlexanderKudr <trevan98@gmail.com>
2022-10-16 22:01:33 +0300

    trying from the start

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,36 +1,43 @@
 import { REST, Routes } from "discord.js";
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
-import * as dotenv from "dotenv";
-dotenv.config();
 
-const token = process.env.DISCORD_TOKEN;
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
-  
-if (token && clientId && guildId) {
-  
-  const commands = [];
-  const commandsPath = path.join(process.cwd(), 'src/commands');
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  for (const file of commandFiles) {
-	  const filePath = path.join(commandsPath, file);
-	  const command = import(filePath);
-	  commands.push(command.data.toJSON());
-  }
 
-  const rest = new REST({ version: "10" }).setToken(token);
 
-  rest
-    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then((data) =>
-      console.log(
-        `Successfully registered ${data.length} application commands.`
-      )
-    )
-    .catch(console.error);
-} else {
-  console.log(`Something is missing`)
-}
+
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+// const token = process.env.DISCORD_TOKEN;
+// const clientId = process.env.CLIENT_ID;
+// const guildId = process.env.GUILD_ID;
+
+// if (token && clientId && guildId) {
+//   const commands = [];
+//   const commandsPath = path.join(process.cwd(), "src/commands");
+//   console.log(commandsPath);
+//   // const commandFiles = fs
+//   //   .readdirSync(commandsPath)
+//   //   .filter((file) => file.endsWith(".js"));
+//   // for (const file of commandFiles) {
+//   //   const filePath = path.join(commandsPath, file);
+//   //   const command = import(filePath);
+//   //   commands.push(command.data.toJSON());
+//   // }
+
+//   const rest = new REST({ version: "10" }).setToken(token);
+
+//   rest
+//     .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+//     .then((data) =>
+//       console.log(
+//         `Successfully registered ${data.length} application commands.`
+//       )
+//     )
+//     .catch(console.error);
+// }
+//      else {
+//   console.log(`Something is missing`)
+// }
--- src/main.js
+++ src/main.js
@@ -8,46 +8,57 @@ dotenv.config();
 
 const token = process.env.DISCORD_TOKEN;
 
-if (token) {
-  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-  client.commands = new Collection();
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
+
+// if (token) {
+//   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//   client.commands = new Collection();
 
 
-  /** commandPath __dirname = \Users\Alex\Desktop\Coding\Projects\JS\hitchbot\src\ + "commands" */
-  const commandsPath = path.join(process.cwd(), "commands");
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+//   /** commandPath __dirname = \Users\Alex\Desktop\Coding\Projects\JS\hitchbot\src\ + "commands" */
+//   const commandsPath = path.join(process.cwd(), "commands");
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
 
-  for (const file of commandFiles) {
-	/**commandPath + file_name => \Users\Alex\Desktop\Coding\Projects\JS\hitchbot\src\commands\ping.js */
-    const filePath = path.join(commandsPath, file);
-	/** export const ping = something */
-    const command = import(filePath);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
-  }
+//   for (const file of commandFiles) {
+// 	/**commandPath + file_name => \Users\Alex\Desktop\Coding\Projects\JS\hitchbot\src\commands\ping.js */
+//     const filePath = path.join(commandsPath, file);
+// 	/** export const ping = something */
+//     const command = import(filePath);
+//     // Set a new item in the Collection
+//     // With the key as the command name and the value as the exported module
+//     client.commands.set(command.data.name, command);
+//   }
 
-  client.once("ready", () => {
-    console.log("Ready!");
-  });
+//   client.once("ready", () => {
+//     console.log("Ready!");
+//   });
 
-  client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+//   client.on("interactionCreate", async (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName)
+// 	const command = interaction.client.commands.get(interaction.commandName)
 
-	if(!command) return;
+// 	if(!command) return;
 
- 	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	} 
-  });
+//  	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	} 
+//   });
 
-  // Login to Discord with your client's token
-  client.login(token);
-}
+//   // Login to Discord with your client's token
+//   client.login(token);
+// }
