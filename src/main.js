d2934321969f063f78cc46078c84bca647aeefc9
Alex <trevan98@gmail.com>
2022-10-21 23:54:59 +0300

    side effects with server

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { token } from "./config/config.js";
 import { events } from "./helpers/eventConsumer.js";
 import { registerCommands } from "./deploy-commands.js";
 import { Client, GatewayIntentBits, Collection } from "discord.js";
+import "./api/server.js";
 
 export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
@@ -10,12 +11,8 @@ const { ready, ifReady, interactionCreate, interactionCreateHandler } = events;
 //register commands from deploy-commands.js file
 registerCommands();
 //reply to commands
-interactionCreateHandler(interactionCreate, client)
+interactionCreateHandler(interactionCreate, client);
 // When the client is ready, this code print: Ready!
 ifReady(ready, client);
 // Login to Discord with client's (bot's) token.
 client.login(token);
-
-
-
-
