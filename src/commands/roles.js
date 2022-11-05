1e3d0737d93d7fc41e6d0bc831dc2727d60df968
Alex <trevan98@gmail.com>
2022-11-05 22:39:27 +0300

    adding lyrics, roles, and roles event/backup

---
+++ src/commands/roles.js
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const roles = {
+  data: new SlashCommandBuilder()
+    .setName("roles")
+    .setDescription("add role to yourself"),
+  execute: async (interaction) => {
+    // interaction.user is the object representing the User who ran the command
+    // interaction.member is the GuildMember object, which represents the user in the specific guild
+    await interaction.reply(
+      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+    );
+  },
+};
