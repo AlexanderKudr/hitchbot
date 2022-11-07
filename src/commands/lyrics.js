045b050035ce29c7ab7ebe5656d33605961fc2c0
Alex <trevan98@gmail.com>
2022-11-07 23:27:07 +0300

    testing lyrics

--- src/commands/lyrics.js
+++ src/commands/lyrics.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
-
+import axios from "axios";
 export const lyrics = {
   data: new SlashCommandBuilder()
     .setName("lyrics")
@@ -7,6 +7,20 @@ export const lyrics = {
   execute: async (interaction) => {
     // interaction.user is the object representing the User who ran the command
     // interaction.member is the GuildMember object, which represents the user in the specific guild
+    const options = {
+      method: 'GET',
+      url: 'https://lyrics-plus.p.rapidapi.com/lyrics/hello/adele',
+      headers: {
+        'X-RapidAPI-Key': 'fb8032f3f6msh70b4ba7315debcep16ca8fjsn88af34360596',
+        'X-RapidAPI-Host': 'lyrics-plus.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
     await interaction.reply(
       `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
     );
